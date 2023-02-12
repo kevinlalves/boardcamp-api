7aac9846100b932a3155f4655ee7baa49c57b88e
Kevin Alves <kevin.alves.dev@gmail.com>
2023-02-12 17:38:00 -0300

    wip

--- src/queries/games.queries.js
+++ src/queries/games.queries.js
@@ -1,20 +1,13 @@
-import camelToSnakeCase from '../utils/functions/camelToSnakeCase.js';
-
 export const listQuery = ({ order, desc }) => (`
-  SELECT
-    id,
-    name,
-    image,
-    stock_total AS "stockTotal",
-    price_per_day AS "pricePerDay"
+  SELECT *
   FROM games
   WHERE name ILIKE $1
-  ORDER BY ${camelToSnakeCase(order)} ${desc ? 'DESC' : 'ASC'}
+  ORDER BY ${order} ${desc ? 'DESC' : 'ASC'}
   OFFSET $2
   LIMIT $3;
 `);
 
 export const addQuery = () => (`
-  INSERT INTO games (name, image, stock_total, price_per_day) VALUES
+  INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES
   ($1, $2, $3, $4);
 `);
--- src/queries/rentals.queries.js
+++ src/queries/rentals.queries.js
@@ -1,60 +1,58 @@
-import camelToSnakeCase from '../utils/functions/camelToSnakeCase.js';
-
 export const listQuery = ({ customerId, gameId, status, startDate, order, desc }) => (`
   SELECT
     rentals.id,
-    rentals.customer_id AS "customerId",
-    rentals.game_id AS "gameId",
-    rentals.rent_date AS "rentDate",
-    rentals.days_rented AS "daysRented",
-    rentals.return_date AS "returnDate",
-    rentals.original_price AS "originalPrice",
-    rentals.delay_fee AS "delayFee",
+    rentals."customerId",
+    rentals."gameId",
+    rentals."rentDate",
+    rentals."daysRented",
+    rentals."returnDate",
+    rentals."originalPrice",
+    rentals."delayFee",
     json_build_object(
-      'id', rentals.customer_id,
+      'id', rentals."customerId",
       'name', customers.name
     ) AS customer,
     json_build_object(
-      'id', rentals.game_id,
+      'id', rentals."gameId",
       'name', games.name
     ) AS game
   FROM rentals
-  JOIN games ON rentals.game_id=games.id
-  JOIN customers ON rentals.customer_id=customers.id
+  JOIN games ON rentals."gameId" = games.id
+  JOIN customers ON rentals."customerId" = customers.id
   WHERE 1=1
-  ${customerId ? `AND rentals.customer_id=${customerId}` : ''}
-  ${gameId ? `AND rentals.game_id=${gameId}` : ''}
-  ${status ? `AND rentals.return_date IS ${status === 'closed' ? 'NOT' : ''} NULL` : ''}
-  ${startDate ? `AND rentals.rent_date >= ${startDate}::date` : ''}
-  ORDER BY ${camelToSnakeCase(order)} ${desc === 'true' ? 'DESC' : ''}
+  ${customerId ? `AND rentals."customerId"=${customerId}` : ''}
+  ${gameId ? `AND rentals."gameId"=${gameId}` : ''}
+  ${status ? `AND rentals."returnDate" IS ${status === 'closed' ? 'NOT' : ''} NULL` : ''}
+  ${startDate ? `AND rentals."rentDate" >= ${startDate}::date` : ''}
+  ORDER BY ${order} ${desc === 'true' ? 'DESC' : ''}
   OFFSET $1
   LIMIT $2;
 `);
 
 export const createReadQuery = () => (`
   SELECT
-    games.stock_total AS "totalNumOfGames",
-    games.price_per_day AS "gamePricePerDay",
-    COUNT(rentals.game_id) AS "numGamesRented"
+    games."stockTotal" AS "totalNumOfGames",
+    games."pricePerDay" AS "gamePricePerDay",
+    COUNT(rentals."gameId") AS "numGamesRented"
   FROM games
-  LEFT JOIN rentals ON rentals.game_id = games.id
+  LEFT JOIN rentals ON rentals."gameId" = games.id
   WHERE games.id = $1
   GROUP BY games.id;
 `);
 
 export const createWriteQuery = () => (`
-  INSERT INTO rentals (customer_id, game_id, days_rented, original_price) VALUES
+  INSERT INTO rentals ("customerId", "gameId", "daysRented", "originalPrice") VALUES
   ($1, $2, $3, $4);
 `);
 
 export const closeQuery = () => (`
   UPDATE rentals
   SET
-    return_date = NOW(),
-    delay_fee = GREATEST(0, (EXTRACT(day FROM NOW()-rentals.rent_date)::int2 - rentals.days_rented) * games.price_per_day)
+    "returnDate" = NOW(),
+    "delayFee" = GREATEST(0, (EXTRACT(day FROM NOW()-rentals."rentDate")::int4 - "rentals.daysRented") * games."pricePerDay")
   FROM games
   WHERE rentals.id = $1
-  AND games.id = rentals.game_id;
+  AND games.id = rentals."gameId";
 `);
 
 export const deleteQuery = () => (`
