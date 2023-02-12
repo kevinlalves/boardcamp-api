99b230f1239eff777182b7410a51aeb662787284
Kevin Alves <kevin.alves.dev@gmail.com>
2023-02-12 16:16:54 -0300

    wip

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URI,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
 });
 
